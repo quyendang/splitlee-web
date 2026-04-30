e48b94a5ae22dfafcaaf28342c9f60e22378a261
quyen.eth <dangnquyen@hotmail.com>
2026-04-30 18:44:20 +0700

    Fix share route asset fetch

--- functions/s/[id].js
+++ functions/s/[id].js
@@ -1,5 +1,5 @@
 export async function onRequest(context) {
   const url = new URL(context.request.url);
-  const assetURL = new URL('/index.html', url);
+  const assetURL = new URL('/', url);
   return context.env.ASSETS.fetch(assetURL.toString());
 }
